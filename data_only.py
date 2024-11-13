cbe3be46a01bfe99cd2b7ee0832bbfb40585642d
Kiryonn <joshmachu@gmail.com>
2024-11-13 13:08:13 +0100

    made help messages more meaningful

--- data_only.py
+++ data_only.py
@@ -23,7 +23,7 @@ def start_test(show_res: bool = False, output: str = None, *args, **kws):
 	generations_tests.on_progression_changed += print_progression
 	results = generations_tests.start(*args, **kws)
 	generations_tests.on_progression_changed -= print_progression
-	print('\n') # put some space after progression bar
+	print('\n')  # put some space after progression bar
 
 	if show_res:
 		generations_tests.result_printer(results)
@@ -38,10 +38,10 @@ def start_test(show_res: bool = False, output: str = None, *args, **kws):
 def main():
 	import argparse
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-s", "--size", nargs=2, type=int, help="the size of the maze")
-	parser.add_argument("-i", "--iter", default=1000, type=int, help="the number of iterations")
-	parser.add_argument("-r", "--show_res", action="store_true", help="shows a summary of the results")
-	parser.add_argument('-o', '--output', default=None, help="saves the results in a specified file")
+	parser.add_argument("-s", "--size", default=(16, 16), nargs=2, metavar=('ROWS', 'COLS'), type=int, help="sets the size (rows then columns) of the mazes")
+	parser.add_argument("-i", "--iter", default=1000, type=int, help="the number of iterations the test should run (more = longer time but more meaningful stats)")
+	parser.add_argument("-r", "--show_res", action="store_true", help="shows a summary of the results at the end of the tests")
+	parser.add_argument('-o', '--output', default=None, help="saves the results (json format) in a specified file to be processed later")
 	namespace = parser.parse_args()
 	args = [namespace.show_res, namespace.output]
 	kws = {"size": tuple(namespace.size), "nb_iterations": namespace.iter}
